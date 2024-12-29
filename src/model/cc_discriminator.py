07f6564e43ba4153023260d645844956d581149b
OTRAY271 <mojayarto@gmail.com>
2024-12-29 11:22:06 +0900

    refac: delete old ccdiscriminator

--- src/model/cc_discriminator.py
+++ src/model/cc_discriminator.py
@@ -41,37 +41,6 @@ class BaseCCDiscriminator(nn.Module):
 
 class MNISTCCDiscriminator(BaseCCDiscriminator):
     def __init__(self):
-        x_layers = nn.Sequential(
-            self._conv_block(1, 16, kernel_size=4, stride=2, dropout=0.2),
-            self._conv_block(16, 32, kernel_size=4, stride=1),
-            self._conv_block(32, 64, kernel_size=4, stride=2),
-            self._conv_block(64, 128, kernel_size=4, stride=1),
-            self._conv_block(128, 256, kernel_size=1, stride=1),
-        )
-
-        xx_layers = nn.Sequential(
-            self._conv_block(512, 512, kernel_size=1, stride=1),
-            self._conv_block(512, 512, kernel_size=1, stride=1),
-            nn.Conv2d(512, 1, kernel_size=1, stride=1),
-        )
-
-        super().__init__(x_layers, xx_layers)
-
-    def _conv_block(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: int,
-        stride: int,
-        dropout: float = 0.5,
-    ) -> nn.Module:
-        return super()._conv_block(
-            in_channels, out_channels, kernel_size, stride, dropout, 0.1
-        )
-
-
-class MNISTCCDiscriminator2(BaseCCDiscriminator):
-    def __init__(self):
         super().__init__(None, None)
 
         self.layers = nn.Sequential(
